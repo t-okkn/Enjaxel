0d057ae206083d43788e75e8262ff83e4bdb6fa2
t-okkn <t-okukubo@outlook.jp>
2019-07-30 19:55:58 +0900

    [feat] NetworkExceptionを新規追加

---
+++ src/Communication/NetworkException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enjaxel.Communication
+{
+    /// <summary>
+    /// ネットワークの接続等に関して問題が発生した場合に発生する例外
+    /// </summary>
+    public class NetworkException : Exception
+    {
+        /// <summary>
+        /// ネットワークの接続等に関して問題が発生した場合に発生します
+        /// </summary>
+        /// <param name="Message"></param>
+        public NetworkException(string Message) : base(Message) { }
+    }
+}
